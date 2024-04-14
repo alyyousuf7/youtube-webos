f98d6cb4cfd88972214de3506420e50cd7fd45f6
Ali Yousuf <aly.yousuf7@gmail.com>
2024-04-14 04:54:27 -0400

    Remove test remote keys

--- src/Remote/constants.ts
+++ src/Remote/constants.ts
@@ -7,8 +7,8 @@ export enum RemoteKey {
 }
 
 export const RemoteKeyMap: Record<RemoteKey, number[]> = {
-  [RemoteKey.RED]: [403, 166, 82],
-  [RemoteKey.GREEN]: [404, 172, 71],
+  [RemoteKey.RED]: [403, 166],
+  [RemoteKey.GREEN]: [404, 172],
   [RemoteKey.YELLOW]: [405, 170],
   [RemoteKey.BLUE]: [406, 167],
   [RemoteKey.BACK]: [27],
